aa31e34d01fd1fa494c11ff0661d5373c30b90b4
Zahid Parvaiz <Zahidparvaiz.zpk@gmail.com>
2023-02-14 13:08:28 +0530

    Added AC

--- src/App.jsx
+++ src/App.jsx
@@ -51,32 +51,40 @@ function App() {
         setScreen(res);
     };
 
+    const clear = () => {
+        setScreen("");
+        setPrev("");
+    };
+
     return (
         <div className="App">
             <h2>Calculator</h2>
             <h4>Zahid Parvaiz</h4>
             <div className="screen">{screen}</div>
             <br />
+            <button className="clear" onClick={clear}>AC</button>
+            <br />
             <button className="num" onClick={() => setScreen(p => p + 7)}>7</button>
             <button className="num" onClick={() => setScreen(p => p + 8)}>8</button>
             <button className="num" onClick={() => setScreen(p => p + 9)}>9</button>
-            <button className="num" onClick={mul}>X</button>
+            <button className="op" onClick={mul}>X</button>
             <br />
             <button className="num" onClick={() => setScreen(p => p + 4)}>4</button>
             <button className="num" onClick={() => setScreen(p => p + 5)}>5</button>
             <button className="num" onClick={() => setScreen(p => p + 6)}>6</button>
-            <button className="num" onClick={sub}>-</button>
+            <button className="op" onClick={sub}>-</button>
             <br />
             <button className="num" onClick={() => setScreen(p => p + 1)}>1</button>
             <button className="num" onClick={() => setScreen(p => p + 2)}>2</button>
             <button className="num" onClick={() => setScreen(p => p + 3)}>3</button>
-            <button className="num" onClick={add}>+</button>
+            <button className="op" onClick={add}>+</button>
             <br />
             <button className="num" onClick={() => setScreen(p => p + 0)}>0</button>
-            <button className="num" onClick={() => setScreen(p => p + ".")}>.</button>
-            <button className="num" onClick={div}>/</button>
-            <button className="num" onClick={calculate}>=</button>
+            <button className="num" onClick={() => !screen.includes(".") && setScreen(p => p + ".")}>.</button>
+            <button className="op" onClick={div}>/</button>
+            <button className="op" onClick={calculate}>=</button>
             <br />
+
 
 
         </div>
