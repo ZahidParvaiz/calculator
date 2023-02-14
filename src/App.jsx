2aad3300d05f1f2f31fd244b7663356981676f9d
Zahid Parvaiz <Zahidparvaiz.zpk@gmail.com>
2023-02-14 10:02:59 +0530

    Added UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,37 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h2>Calculator</h2>
+            <div className="screen">42123</div>
+            <br />
+            <button className="num">7</button>
+            <button className="num">8</button>
+            <button className="num">9</button>
+            <button className="num">X</button>
+            <br />
+            <button className="num">4</button>
+            <button className="num">5</button>
+            <button className="num">6</button>
+            <button className="num">-</button>
+            <br />
+            <button className="num">1</button>
+            <button className="num">2</button>
+            <button className="num">3</button>
+            <button className="num">+</button>
+            <br />
+            <button className="num">0</button>
+            <button className="num">.</button>
+            <button className="num">/</button>
+            <button className="num">=</button>
+            <br />
+
+
+        </div>
+    )
 }
 
 export default App
